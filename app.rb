06b93b0f3c232f317fa512bbe934ad7886a28ad5
Sergio Gonzalez <sergio.gonzalez04@utrgv.edu>
2018-11-17 16:31:00 -0600

    first commit

---
+++ app.rb
@@ -0,0 +1,39 @@
+require "sinatra"
+require "data_mapper"
+
+# need install dm-sqlite-adapter
+# if on heroku, use Postgres database
+# if not use sqlite3 database I gave you
+if ENV['DATABASE_URL']
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+else
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
+end
+
+class Video
+	include DataMapper::Resource
+
+	property :id, Serial
+	#fill in the rest
+end
+
+DataMapper.finalize
+Video.auto_upgrade!
+
+
+#the following urls are included in authentication.rb
+# GET /login
+# GET /logout
+# GET /sign_up
+
+# authenticate! will make sure that the user is signed in, if they are not they will be redirected to the login page
+# if the user is signed in, current_user will refer to the signed in user object.
+# if they are not signed in, current_user will be nil
+
+get "/" do
+	erb :index
+end
+
+get "/perfumes" do
+	return "Work in progress..."
+end
