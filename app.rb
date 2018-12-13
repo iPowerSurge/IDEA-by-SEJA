61bd21d86fb76360ad0854af1b8323099f95fb0b
Sergio Gonzalez <sergio.gonzalez04@utrgv.edu>
2018-12-13 11:43:16 -0600

    Done.

--- app.rb
+++ app.rb
@@ -33,6 +33,36 @@ if User.all(administrator: true).count == 0
 	u.save
 end
 
+def make_cart
+	session[:cart]={}
+	flash[:error] = "Had to make new cart"
+	redirect "/perfumes"
+end
+
+def add_to_cart(pid)
+	make_cart if !session.has_key?(:cart)
+
+	if(session[:cart][pid])
+		session[:cart][pid] += 1
+	else
+		session[:cart][pid] = 1
+	end
+end
+
+def remove_from_cart(pid)
+	make_cart if !session[:cart]
+	if(session[:cart][pid] != nil && session[:cart][pid] > 1)
+		session[:cart][pid] = session[:cart][pid] - 1
+	else
+		session[:cart].delete(pid)
+	end
+end
+
+def show_cart
+	@perfume.each do |cart, pid|
+		puts "#{cart}:#{pid}"
+	end
+end
 #the following urls are included in authentication.rb
 # GET /login
 # GET /logout
@@ -72,25 +102,34 @@ post "/perfume/create" do
 	end
 end
 
-get "/add_to_cart" do
-	p = Perfume.get(pid.to_i)
-	if(session[:cart][p.name])
-		session[:cart][p.name] += 1
+get "/perfume/delete/:pid" do
+	pid = params[:pid]
+	p = Perfume.get(pid)
+	if p
+		flash[:success] = "Successfully deleted #{p.name}"
+		p.destroy
+		session[:cart].delete(pid)	
 		redirect "/perfumes"
 	else
-		session[:cart][p.name] = 0
+		flash[:error] = "Could not find the product."
 		redirect "/perfumes"
 	end
 end
 
-get "/remove_from_cart" do
-	p = Perfume.get(pid.to_i)
-	if(session[:cart][p.name] != nil)
-		session[:cart][p.name] = session[:cart][p.name] - 1
-		redirect "/perfumes"
-	else
-		session[:cart][p.name] = 0
-	end
+get "/add_to_cart/:pid" do
+	pid = params[:pid].to_i
+	p = Perfume.get(pid)
+	add_to_cart(pid)
+	flash[:success] = "#{p.name} successfully added to cart."
+	redirect "/perfumes"
+end
+
+get "/remove_from_cart/:pid" do
+	pid = params[:pid].to_i
+	p = Perfume.get(pid)
+	remove_from_cart(pid)
+	flash[:success] = "#{p.name} successfully removed cart."
+	redirect "/perfumes"
 end
 
 get "/cart" do
